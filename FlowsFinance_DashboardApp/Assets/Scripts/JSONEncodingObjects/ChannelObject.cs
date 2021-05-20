ea66550cd792ce013fba557bdd74c6fa2ab1edfe
atomicdomb <atomicdomb@gmail.com>
2021-05-20 16:20:49 +0200

    Added ChannelObject

---
+++ FlowsFinance_DashboardApp/Assets/Scripts/JSONEncodingObjects/ChannelObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChannelObject
+{
+    public string channel_identifier;
+    public string partner_address;
+    public string token_address;
+    public string token_network_address;
+    public string balance;
+    public string total_deposit;
+    public string total_withdraw;
+    public string state;
+    public string settle_timeout;
+    public string reveal_timeout;
+    public ChannelObject(){}
+}

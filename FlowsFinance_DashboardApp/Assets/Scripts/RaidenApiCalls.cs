00fe48aa22a5b2fe8d408a64cb7b4110746334cd
atomicdomb <atomicdomb@gmail.com>
2021-05-20 15:10:14 +0200

    Implemented Restful Calls
    Implemented a series of HTTP GET requests to the Raiden Api

--- FlowsFinance_DashboardApp/Assets/Scripts/RaidenApiCalls.cs
+++ FlowsFinance_DashboardApp/Assets/Scripts/RaidenApiCalls.cs
@@ -1,18 +1,87 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class RaidenApiCalls : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private string jsonResult;
+    private bool isReady=true;
+    private string ipAddress;
+    private string errorInfo;
+    //TODO remove MonoBehaviour once initial tests are complete
     void Start()
     {
-        
+
+    }
+    public void setIpAddress(string ip){
+      ipAddress = ip;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void getAddress(){
+      string uri = "http://"+ipAddress+":5001/api/v1/address";
+      startGetRoutineIfReady(uri);
+    }
+
+    public void getVersion(){
+      string uri = "http://"+ipAddress+":5001/api/v1/version";
+      startGetRoutineIfReady(uri);
+    }
+
+    public void getStatus(){
+      string uri = "http://"+ipAddress+":5001/api/v1/status";
+      startGetRoutineIfReady(uri);
+    }
+
+    public void getSettings(){
+      string uri = "http://"+ipAddress+":5001/api/v1/settings";
+      startGetRoutineIfReady(uri);
+    }
+
+    public void getContracts(){
+      string uri = "http://"+ipAddress+":5001/api/v1/contracts";
+      startGetRoutineIfReady(uri);
+    }
+
+    public void getTokens(){
+      string uri = "http://"+ipAddress+":5001/api/v1/tokens";
+      startGetRoutineIfReady(uri);
+    }
+
+    public void getTokenAddress(string address){
+      string uri = "http://"+ipAddress+":5001/api/v1/tokens/"+address;
+      startGetRoutineIfReady(uri);
+    }
+
+    public void getTokenPartners(string address){
+      string uri = "http://"+ipAddress+":5001/api/v1/tokens/"+address+"/partners";
+      startGetRoutineIfReady(uri);
+    }
+
+    private void startGetRoutineIfReady(string uri){
+      if(isReady){
+        StartCoroutine(RestfulGetRequest(uri));
+      }
+    }
+
+    IEnumerator RestfulGetRequest(string uri)
     {
-        
+        isReady = false;
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(uri))
+        {
+            // Request and wait for the desired page.
+            yield return webRequest.SendWebRequest();
+
+            string[] pages = uri.Split('/');
+            int page = pages.Length - 1;
+
+            if(webRequest.isNetworkError || webRequest.isHttpError){
+              Debug.LogError(webRequest.error);
+              errorInfo = webRequest.error;
+            }else{
+              jsonResult = webRequest.downloadHandler.text;
+              isReady = true;
+            }
+        }
     }
 }
